82512d34c84cc0cc3b927bcb7457525cc4c7f7a0
Shashank Srivastava <shashank.ssriv@gmail.com>
2019-11-01 16:19:11 +0530

    Add Python test file for Selenium

---
+++ integration-test.py
@@ -0,0 +1,40 @@
+import time
+import unittest
+import os
+import logging
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class PythonOrgSearchTest1(unittest.TestCase):
+
+    def setUp(self):
+        print("In setUp")
+        caps = {'browserName': 'chrome'}
+        self.browser = webdriver.Remote(
+            command_executor='http://localhost:4444/wd/hub',
+            desired_capabilities=caps)
+        self.logger = logging
+        self.logger.info("About to call a test")
+
+    def test_simple(self):
+        print("In simple")
+        self.logger.info("In simple")
+        browser = self.browser
+        browser.get('http://localhost:8080/employees-app/')
+        self.assertIn("Home", browser.title)
+        search_box = browser.find_element_by_name('employeeName')
+        search_box.send_keys('John')
+        search_box.send_keys(Keys.RETURN)
+        time.sleep(3) # simulate long running test
+        self.assertIn('John', browser.page_source)
+
+    def tearDown(self):
+        print("In tearDown")
+        self.browser.quit() # quit vs close?
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='selenium-plugin.log',level=logging.INFO)
+    logging.info("About to call main")
+    unittest.main()
